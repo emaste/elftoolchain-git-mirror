69a1a6d70def701f804381b4d0f5e45c42c3394e
Ed Maste <emaste@freebsd.org>
2020-02-15 00:54:59 -0500

    setup.sh: build svn2git if necessary

--- setup.sh
+++ setup.sh
@@ -2,8 +2,22 @@
 # Set up ELF Tool Chain svn mirror for svn2git
 # Ref: http://www.microhowto.info/howto/mirror_a_subversion_repository.html
 
+set -e
+
 cd $(dirname $0)
 dir=$(pwd)
+
+if [ ! -e svn2git/svn-all-fast-export ]; then
+	if ! which qmake >/dev/null; then
+		echo "qmake required to build svn2git" >&2
+		exit 1
+	fi
+	echo "building svn2git"
+	cd svn2git
+	qmake && make
+	cd ..
+	echo
+fi
 
 if [ -d svn-repo ]; then
 	echo "svnsync directory $dir/svn-repo exists"
