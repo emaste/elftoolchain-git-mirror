b19770f2c83a15957336c7bf1a52092a6a8477e0
Ed Maste <emaste@freebsd.org>
2020-02-15 00:41:42 -0500

    mirror.sh: add script to run svnsync and svn2git

---
+++ mirror.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+dir=$(pwd)
+
+SVN2GIT=$dir/svn2git/svn-all-fast-export
+
+# sync svn repo
+svnsync sync file://$dir/svn-repo
+
+# git conversion
+$SVN2GIT $maxrev --fast-import-timeout 3600 --add-metadata-notes \
+    --rules rules \
+    --identity-map identity.map \
+    svn-repo
